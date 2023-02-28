bfd79f0714b931edf187ee1f985f1250a731068e
Dugbazah Redeemer <dugbazahredeemer@gmail.com>
2023-02-28 23:03:01 +0000

    a function to reverse the stings

--- largest_num.js
+++ largest_num.js
@@ -22,3 +22,21 @@ function largestArray(numArray) {
   let evenNumbers = [2,4,6,8,10];
   console.log(largestArray(evenNumbers));// this will print 10(the largest).
 // done.
+
+
+//Exercise(5) number two(2)
+//Write a function that takes a string as an argument and returns the string reversed.
+
+// This is the Pseudocode
+// -Define an array of strings
+// -Loop through with arguments
+// -Finally return the result with reversed method 
+
+// the code
+function stringArgument() {
+  for (let stringsOfArray of arguments) 
+  return stringsOfArray.reverse(); // this function is use to reverse the array to start from the end.
+}
+const array = ['do','go','come'];
+console.log(stringArgument(array));
+// done.
