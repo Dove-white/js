d098e8326045055ef5808c4536c820404790d4af
Dugbazah Redeemer <dugbazahredeemer@gmail.com>
2023-03-06 12:01:53 +0000

    a function to return the sum

---
+++ total_sum.js
@@ -0,0 +1,12 @@
+//Exercise(8) number two(2)
+//Write a function that takes an array of numbers and returns the sum of all the numbers that are divisible by 3 or 5.
+// -Define an array of numbers
+// -Initialize a variable to hold filter method of divisible by 3 or 5
+// -Finally return variable with reduce method to add
+
+const numbers = [10,15,17,31];
+function totalSum(num) {
+  const divBy5Or2 = num.filter(number => (number % 3 === 0) || (number % 5 === 0));
+  return divBy5Or2.reduce((divBy5,divBy2) => divBy5+divBy2);
+}
+console.log(totalSum(numbers));
