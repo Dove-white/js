791fff61266a499e9afa8f19f1d091452b86c321
Dugbazah Redeemer <dugbazahredeemer@gmail.com>
2023-03-22 08:11:25 +0000

    changes to return not prime for 1 and 2

--- prime_number.js
+++ prime_number.js
@@ -1,12 +1,13 @@
 //Write a function to check if a number is prime.
 // -Loop through the number to include two
-// -Set an IF statement to return not prime, if number divisible by index. 
+// -Set an IF statement to return not prime, if number divisible by index and less than two. 
 // -Finally return true.
 
 function primeNum(num) {
     for (let i = 2; i < num; i++) {
         if (num % i === 0) return 'notPrime';
     }
+    if (num < 2) return 'notPrime';
     return 'prime';
 }
 
