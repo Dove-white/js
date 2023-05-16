e6bd1371f1049d48efc8bab9ff54a738da01d129
Dove-White <dugbazahredeemer@gmail.com>
2023-05-16 10:00:25 +0000

    added a traditional for loop

--- bill_spilting.js
+++ bill_spilting.js
@@ -1,5 +1,10 @@
 function isBillSplitCorrect(items,annasAmount,briansAmount) {
-    let totalItems = items.reduce((value,cerrentValue) => value+cerrentValue);
+    // let totalItems = items.reduce((value,currentValue) => value+currentValue);
+    let totalItems = 0;
+
+    for(let i = 0; i < items.length; i++) {
+        totalItems += items[i];
+    }
 
     briansAmount;
     let annasPortion = totalItems / 2;
