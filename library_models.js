c0af6c82f1f6c0e2dbf930ab4f21c9e798736e56
Dove-White <dugbazahredeemer@gmail.com>
2023-05-20 14:11:04 +0000

    correction on listing and added counting

--- library_models.js
+++ library_models.js
@@ -1,40 +1,60 @@
 /*
 Exercise 5
-A JavaScript program that models a library, and should have a function that will add, remove and also list the proparties in it.
+A JavaScript program that models a library, and should have a function that will add, remove and also list the properties in it.
 
 // For adding
 -Define an array of object.
--Initialize a vareable to hold the element and add it to array of object with push method.
+-Initialize a variable to hold the element and add it to array of object with push method.
 
 // For removing
--Initialize another vareable to hold one of the value in the object to remove with a filter method to return not equle to the value.
+-Initialize another variable to hold one of the value in the object to remove with a filter method to return not equal to the value.
 
 // For listing
--Define a callback function with the map method.
--Loop through with for in loop.
--Finally return the proparties and the proparties values.
+-Define a callback function with the forEach method.
+-Finally print the callback.
+
+// For counting
+-Initialize a variable to hold a push method and finally print the variable.
 */
 
 let library = [
-    {title:'At The Heart Of Leadership', author:'Joshua Freedman', yearOfPubilcation:2019},
-    {title:'Forgive Yourself These Tiny Acts Of Self-Destrgaction', author:'Jared Singer', yearOfPubilcation:2007},
-    {title:'Being The Best Version Of You', author:'Charlotte Moore', yearOfPubilcation:2010},
-    {title:'The Future', author:'Neil Hilborn', yearOfPubilcation:2018}, 
+  {
+    title: "At The Heart Of Leadership",
+    author: "Joshua Freedman",
+    yearOfPublication: 2019,
+  },
+  {
+    title: "Forgive Yourself These Tiny Acts Of Self-Destruction",
+    author: "Jared Singer",
+    yearOfPublication: 2007,
+  },
+  {
+    title: "Being The Best Version Of You",
+    author: "Charlotte Moore",
+    yearOfPublication: 2010,
+  },
+  { title: "The Future", author: "Neil Hilborn", yearOfPublication: 2018 },
 ];
 
 // For adding
-let p = {title:'You Better Be Lighting', author:'Andrea Gibson', yearOfPubilcation:2015};
-library.push(p);
+let bookAdded = {
+  title: "You Better Be Lighting",
+  author: "Andrea Gibson",
+  yearOfPublication: 2015,
+};
+library.push(bookAdded);
 console.log(library);
 
 // For removing
-const propartiesRemove = 'The Future';
-let removeMethod = library.filter((proparties) => proparties.title !== propartiesRemove);
+const propertiesRemove = "The Future";
+const removeMethod = library.filter(
+  (properties) => properties.title !== propertiesRemove
+);
 console.log(removeMethod);
 
+// For counting
+let numberOfBooks = library.push();
+console.log("There are " + numberOfBooks + " books in the library");
 
 // For listing
-removeMethod.map(value=> {
-    for(let proparties in value)
-    console.log(`${proparties}:${value[proparties]}`);
-});
+library.forEach((properties) => console.log(properties));
